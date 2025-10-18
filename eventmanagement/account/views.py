b2f74a562a1fb7714f0e533b704385fbc106fa68
russelguinares <russelguinarez@gmail.com>
2025-10-18 10:59:02 +0800

    with view and url

--- eventmanagement/account/views.py
+++ eventmanagement/account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request):
+    return render(request, 'index.html')
+
+def login_view(request):
+    return render(request, 'login.html')
